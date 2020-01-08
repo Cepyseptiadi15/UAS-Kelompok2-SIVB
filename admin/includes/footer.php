ee48c1896af78a7c3519ae84df99ce1da7013979
Cepyseptiadi15 <Cepyseptiadi9@gmail.com>
2020-01-08 15:16:51 +0700

    UAS

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <strong>Copyright &copy; 2019 <a href="">R.Rozali</a></strong>
+</footer>
